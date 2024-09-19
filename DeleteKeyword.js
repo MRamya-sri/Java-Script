b8f0dce66fa47a82a4c7e812011e40b8411af83b
MRamya-sri <rsri40170@gmail.com>
2024-09-19 19:34:00 +0530

    javascript

---
+++ DeleteKeyword.js
@@ -0,0 +1,3 @@
+let key = {course:"javascript", duration: 30};
+delete key.duration;
+console.log(key)
--- globalVariable.js
+++ globalVariable.js
@@ -8,3 +8,5 @@ function myFunction(){
 }
 
 console.log("Can access outside the function also : ", globeVar);
+
+/*  In contrast, global variables are the variables that define outside of functions. These variables have a global scope, so they can be used by any function without passing them to the function as parameters. */
